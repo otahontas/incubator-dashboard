47daea8bd2b787cad9d5411d713ff8aa3dc8c0db
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-19 19:37:19 +0200

    remove boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import { ChakraProvider } from "@chakra-ui/react"
-
-
 
 ReactDOM.render(
   <React.StrictMode>
