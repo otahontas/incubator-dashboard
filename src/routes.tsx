6e449a66ad084dba8571f3e9c0fbea37de3255c0
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-19 22:45:45 +0200

    Add form

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { RouteObject }from 'react-router-dom'
 import { LoginView } from './pages/LoginView'
+import { FeedbackView } from "./pages/FeedbackView"
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <LoginView />
-    }
+    },
+  { 
+    path: "/form",
+    element: <FeedbackView />
+  }
 ]
 
