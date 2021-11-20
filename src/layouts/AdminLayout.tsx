7ce6bc17144422f7ce7bcc2c26d8c07974663b92
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-20 03:15:07 +0200

    Add navbar

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,12 +1,15 @@
 import { Box, Center } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
+import OverallLayout from "./OverallLayout"
 
 export default () => {
   return (
-    <Box h="100vh" mx="32">
-      <Center>
-        <Outlet />
-      </Center>
-    </Box>
+    <OverallLayout>
+      <Box h="100vh" mx="32">
+        <Center>
+          <Outlet />
+        </Center>
+      </Box>
+    </OverallLayout>
   )
 }
--- src/pages/admin/Home.tsx
+++ src/pages/admin/Home.tsx
@@ -13,35 +13,11 @@ import {
 } from "@chakra-ui/react"
 import Roadmaps from "./RoadmapTemplate"
 
-const Header = () => {
-  return (
-    <Box
-      bg={useColorModeValue("blue.50", "blue.900")}
-      w="100%"
-      p={4}
-      color="black"
-    >
-      <Text
-        textTransform={"uppercase"}
-        color={"blue.400"}
-        fontWeight={600}
-        fontSize={"sm"}
-        bg={useColorModeValue("blue.50", "blue.900")}
-        p={2}
-        alignSelf={"flex-start"}
-        rounded={"md"}
-      >
-        Admin dashboard
-      </Text>
-    </Box>
-  )
-}
 
 export default () => {
   return (
     <>
       <Container maxW="container.md">
-        <Header />
         <Stack spacing={12}>
           <Heading>Manage the program</Heading>
           <Tabs isFitted variant="enclosed" colorScheme="blue">
