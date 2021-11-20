018eb69f2b03fc8886927f8abd2cfb5110f01692
Teemu Helenius <heleniusteemu@gmail.com>
2021-11-20 12:20:39 +0200

    Revert "fix login view"
    This reverts commit ec6ba2c684c9881f84d5379a0529bfb87293e4d1.

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,20 @@ import Loading from "./sharedComponents/Loading"
 import { Button } from "@chakra-ui/react"
 import ErrorBoundary from "./sharedComponents/ErrorBoundary"
 
-
+// plz fix this, it's ugly
+const LoginButton = () => {
+  const auth = useAuth()
+  const login = async () => {
+    await signInWithRedirect(auth, new GoogleAuthProvider())
+  }
+  return <Button onClick={login}>LOGIN</Button>
+}
 
 const App = () => {
   const appRoutes = useRoutes(routes)
   const { status, data: signInResult } = useSigninCheck()
   if (status === "loading") return <Loading />
-
+  if (!signInResult.signedIn) return <LoginButton />
   return (
     <ErrorBoundary>
         {appRoutes}
