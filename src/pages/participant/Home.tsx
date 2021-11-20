e9673aba1a103853c9b274f8a943544cd05ec38b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-20 22:43:30 +0200

    stuff

--- src/pages/participant/Home.tsx
+++ src/pages/participant/Home.tsx
@@ -1,17 +1,16 @@
 import { useEffect } from "react"
 import { WeeklyUpdateView } from "./WeeklyUpdate"
-import RoadmapView from "./RoadmapView"
 import {
   Box,
   Button,
-  Center,
   Divider,
   Text,
   Flex,
   Heading,
   HStack,
-  SimpleGrid,
+  Stack,
   VStack,
+  Progress,
 } from "@chakra-ui/react"
 import { AiOutlineTeam } from "react-icons/ai"
 import { useState } from "react"
@@ -38,19 +37,26 @@ export default ({ }) => {
 
   return (
     <>
-      <Flex direction="row-reverse">
+      <Flex direction="row" alignItems="center">
+        <div style={{ width: '100%', marginRight: 'auto', paddingLeft: '16px', paddingRight: '16px'}}>
+          <Progress colorScheme="green" height="20px" value={60} borderRadius={8} width="100%"/>
+        </div>
         <Button leftIcon={<AiOutlineTeam />}>Switch to team view</Button>
       </Flex>
 
-      <HStack spacing="4">
+      <HStack spacing="4" margin={4}>
         {stages.map((stage) => (
-          <Button onClick={() => setCurrentStageId(stage.id)}key={stage.id}>
+          <Button colorScheme="green" variant={currentStageId === stage.id ? "solid" : "ghost"}
+            onClick={() => setCurrentStageId(stage.id)}key={stage.id}>
           {stage.title}{" "}
           </Button>
         ))}
       </HStack>
 
-      <Text>Active stage {activeStage.name}</Text>
+      <Heading as="h6" size="xs">
+        Team ID: {userData.teamId}. Send this to your friends!
+      </Heading>
+
       <Divider py="8" />
       <HStack overflowX="auto">
         {activeStage.milestones.map((milestone, i) => (
@@ -69,10 +75,9 @@ export default ({ }) => {
           </Box>
       ))}
       </HStack>
-      <Heading as="h6" size="xs">
-        Team ID: {userData.teamId}. Send this to your friends!
-      </Heading>
-      <RoadmapView milestones={activeStage.milestones} />
+      <Stack spacing={5} margin={8}>
+        <Heading>Your current progress on this stage:</Heading>
+      </Stack>
       <WeeklyUpdateView />
     </>
   )
