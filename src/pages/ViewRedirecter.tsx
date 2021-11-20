ec6ba2c684c9881f84d5379a0529bfb87293e4d1
Phong Tran <phong.tran@ellie.ai>
2021-11-20 11:29:26 +0200

    fix login view

--- src/pages/ViewRedirecter.tsx
+++ src/pages/ViewRedirecter.tsx
@@ -2,23 +2,37 @@ import { useEffect } from "react"
 import Loading from "../sharedComponents/Loading"
 import useAuthenticatedUser from "../hooks/useAuthenticatedUser"
 import { useNavigate } from "react-router-dom"
+import { useAuth, useSigninCheck } from "reactfire"
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth"
+import { Button } from "@chakra-ui/react"
+
+// plz fix this, it's ugly
+const LoginButton = () => {
+  const auth = useAuth()
+  const login = async () => {
+    await signInWithRedirect(auth, new GoogleAuthProvider())
+  }
+  return <Button onClick={login}>LOGIN</Button>
+}
 
 const ViewRedirecter: React.FC = () => {
   const { status, data } = useAuthenticatedUser()
+  
   const navigate = useNavigate()
   useEffect(() => {
     if (status === 'loading') return
-    if (data.role === 'participant') {
+    if (data?.role === 'participant') {
       navigate('/participant')
     }
-    if (data.role === 'coach') {
+    if (data?.role === 'coach') {
       navigate('/coach')
     }
-    if (data.role === 'admin') {
+    if (data?.role === 'admin') {
       navigate('/admin')
     }
   }, [status])
 
+  if (!data) return <LoginButton />
   // show loader while resolving redirecter
   return <Loading />
 }
