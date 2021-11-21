6cce9b04e6c30e5c6cffb03d30beb492e2b11d9a
Phong Tran <phong.tran@ellie.ai>
2021-11-21 04:23:48 +0200

    change navigation mechanism

--- src/pages/coach/TeamCard.tsx
+++ src/pages/coach/TeamCard.tsx
@@ -1,9 +1,10 @@
-import { Flex, Heading, Box, Text, IconButton } from "@chakra-ui/react"
+import { Flex, Heading, Box, Text, Avatar } from "@chakra-ui/react"
 import { BsArrowsAngleExpand } from "react-icons/bs"
 import { useNavigate } from "react-router-dom"
 import useAllUsers from "../../hooks/useAllUsers"
 import Loading from "../../sharedComponents/Loading"
 import faker from "faker"
+import {Link} from 'react-router-dom'
 export interface TeamCardProps {
   team: Team
 }
@@ -16,14 +17,10 @@ export default (props: TeamCardProps) => {
   if (status === 'loading') return <Loading />
 
   return (
-    <Box p="6" borderWidth="1px" borderRadius="lg">
+    <Box cursor='pointer' onClick={() => navigate(`/coach/${team.id}`, { state: team })} p="6" borderWidth="1px" borderRadius="lg">
       <Flex justify="space-between">
+        <Avatar  />
         <Heading>{team.name}</Heading>
-        <IconButton
-          aria-label="Expand team info"
-          icon={<BsArrowsAngleExpand />}
-          onClick={() => navigate(`/coach/${team.id}`, { state: team })}
-        />
       </Flex>
       <Text> Team members</Text>
       {team.members.map(t => <Text>{
